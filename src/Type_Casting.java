f08ddd7b1f502f681a0c5afe56f499704468902c
Alkalif-code <alkalif347@gmail.com>
2026-02-22 20:05:15 +0530

    type casting added

---
+++ src/Type_Casting.java
@@ -0,0 +1,13 @@
+public class Type_Casting {
+    public static void main(String args[])
+    {
+        int a = 24;
+        byte b=(byte)a;    // byte to integer
+
+        float f = 5.3f;
+        int t = (int)f;   // converting float value into integer value 
+
+        System.out.println(b);
+        System.out.println(t);
+    }
+}
