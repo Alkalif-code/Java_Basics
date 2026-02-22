4aa8710100d1b1f6328028e0a6dc1e081b8bc8ef
Alkalif-code <alkalif347@gmail.com>
2026-02-22 17:30:30 +0530

    first program

---
+++ src/Datatypes_Variables.java
@@ -0,0 +1,9 @@
+public class Datatypes_Variables {
+    public static void main(String[] args) {
+         int Rollno = 5654;
+        String Name = "Alkalif";
+        System.out.println(Rollno);
+        System.out.println(Name);
+    }
+   
+}
