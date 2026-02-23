f41c8360f14286f48af861f270f9daae54ac8d50
Alkalif-code <alkalif347@gmail.com>
2026-02-23 20:23:44 +0530

    added if else

---
+++ src/IF_Else_Statement.java
@@ -0,0 +1,15 @@
+public class IF_Else_Statement{
+    public static void main(String[] args)
+    {
+        int a= 4;
+        int b=5;
+
+        if(a%2==0){  //when this condition valid below code will work otherwise else part work 
+            System.out.println("This is an Even number");
+        }
+        else{
+            System.out.println("This is a Odd Number");
+        }
+
+    }
+}
