89d2ae05a5b39c0c64ca8aa36043541effa1a6de
Alkalif-code <alkalif347@gmail.com>
2026-02-23 20:22:15 +0530

    added relational op

---
+++ src/Relational_Operators.java
@@ -0,0 +1,19 @@
+public class Relational_Operators{
+    public static void main(String[] args){
+        
+        int a = 45;
+        int b = 68;
+
+
+        boolean c = a > b;        //Greater than we can also greater than equal
+        boolean d = a < b;        //less than  we can also lesser than equal
+        boolean e = a==b;        //equal to
+        boolean f = a!=b;        // not equal to
+
+
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
